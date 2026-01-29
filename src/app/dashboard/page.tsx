9d62a8913b80b6f0ded2a82ea219d09a0de5fa96
Jose Daniel Avendaño Morales <joseavendano@politecnicodecolombia.edu.co>
2026-01-29 18:36:36 +0000

    aun sale y tiene esto en console:
    monospace-pa.clients6.google.com/$rpc

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,8 @@
 import AppLayout from "@/components/app-layout";
 import ScheduleView from "@/components/dashboard/schedule-view";
 
+export const maxDuration = 120; // 2 minutes
+
 export default function DashboardPage() {
   return (
     <AppLayout>
